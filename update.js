1589ed1abc93f0f9c42fce5a96c74f9f5c32ab63
Max Zinovik <zinovik@gmail.com>
2022-09-10 07:33:04 +0400

    update bgg games ranks url

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ const https = require("https");
 const fs = require("fs");
 
 const BGG_GAMES_RANKS_URL =
-  "https://bgg-games-ranks.vercel.app/api/get-games?amount=2000";
+  "https://raw.githubusercontent.com/zinovik/bgg-games-ranks-data/main/bgg-games-ranks.json";
 const BGG_GAMES_RANKS_FILE = "./bgg-games-ranks.json";
 
 const request = (url) =>
